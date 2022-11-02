7a86eb72eb68feaf6d47472f3430c508adf4564e
Francisco Benítez <frabenrui1@alum.us.es>
2022-11-02 23:59:15 +0100

    9 successful tests

--- src/main/java/org/springframework/samples/petclinic/product/ProductService.java
+++ src/main/java/org/springframework/samples/petclinic/product/ProductService.java
@@ -2,21 +2,33 @@ package org.springframework.samples.petclinic.product;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class ProductService {
+
+    @Autowired
+    private ProductRepository productRepository;
+
     public List<Product> getAllProducts(){
-        return null;
+        return productRepository.findAll();
     }
 
     public List<Product> getProductsCheaperThan(double price) {
-        return null;
+        return productRepository.findByPriceLessThan(price);
     }
 
     public ProductType getProductType(String typeName) {
-        return null;
+        return productRepository.findProductTypeByName(typeName);
     }
 
     public Product save(Product p){
         return null;       
+    }
+
+    public List<ProductType> findAllProductTypes() {
+        return productRepository.findAllProductTypes();
     }
 
     
--- src/main/java/org/springframework/samples/petclinic/product/ProductTypeFormatter.java
+++ src/main/java/org/springframework/samples/petclinic/product/ProductTypeFormatter.java
@@ -3,22 +3,31 @@ package org.springframework.samples.petclinic.product;
 import java.text.ParseException;
 import java.util.Locale;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.Formatter;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ProductTypeFormatter implements Formatter<ProductType>{
 
+    @Autowired
+    private ProductService productService;
+
     @Override
     public String print(ProductType object, Locale locale) {
         // TODO Auto-generated method stub
-        return null;
+        return object.getName();
     }
 
     @Override
     public ProductType parse(String text, Locale locale) throws ParseException {
         // TODO Auto-generated method stub
-        return null;
+        ProductType findProductType = productService.getProductType(text);
+        if(findProductType != null) {
+            return findProductType;
+        } else {
+            throw new ParseException("Product type not found", 0);
+        }
     }
     
 }
